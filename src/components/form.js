83401fb6c40981a5738cf4e872f3eaa1567f4f72
INS140 <IsaacNSaettel@gmail.com>
2023-02-16 05:33:36 -0800

    added active states

--- src/components/form.js
+++ src/components/form.js
@@ -8,42 +8,110 @@ export default function Form() {
         password: 'Password'
     })
 
+    const [validEmail, setValidEmail] = useState(true)
+
     const handleInputChange = (event) => {
-        const { value, name } = event.target
-        setInputs(values => {return {...values, [name]: value}})
+      const { value, name } = event.target
+      setInputs(values => {return {...values, [name]: value}})
+    }
+
+    const handleEmailChange = async (event) => {
+      const { value, name } = event.target
+      const valid = (/\w+@\w+.com/.test(value)) //working here
+      
+      if (valid) setValidEmail(true)
+      else setValidEmail(false)
+      setInputs(values => {return {...values, [name]: value}})
     }
 
     return (
         <form>
-            <input
-              type="text"
-              value={inputs.first}
-              name="first"
-              onChange={handleInputChange}
-              required
-            />
-            <input
-              type="text"
-              value={inputs.last}
-              name="last"
-              onChange={handleInputChange}
-              required
-            />
-            <input
-              type="email"
-              value={inputs.email}
-              name="email"
-              onChange={handleInputChange}
-              required
-            />
-            <input
-              type="password"
-              value={inputs.password}
-              name="password"
-              onChange={handleInputChange}
-              required
-            />
-            <input type="submit" value="CLAIM YOUR FREE TRIAL"/>
+          {
+            (inputs.first !== '') 
+              ? <div>
+                  <input
+                    type="text"
+                    value={inputs.first}
+                    name="first"
+                    onChange={handleInputChange}
+                  />
+                </div> 
+              : <div>
+                  <input
+                    type="text"
+                    value={inputs.first}
+                    name="first"
+                    onChange={handleInputChange}
+                    className="field-empty"
+                  />
+                  <p className="warning-text"><i>First Name cannot be empty</i></p>
+                </div>
+          }
+          {
+            (inputs.last !== '')
+              ? <div>
+                <input
+                  type="text"
+                  value={inputs.last}
+                  name="last"
+                  onChange={handleInputChange}
+                />
+              </div>
+              : <div>
+                <input
+                  type="text"
+                  value={inputs.last}
+                  name="last"
+                  onChange={handleInputChange}
+                  className="field-empty"
+                />
+                <p className="warning-text"><i>Last Name cannot be empty</i></p>
+              </div>
+          }
+          {
+            (validEmail)
+              ? <div>
+                  <input
+                    type="email"
+                    value={inputs.email}
+                    name="email"
+                    onChange={handleEmailChange}
+                  />
+              </div>
+              : <div>
+                <input
+                  type="email"
+                  value={inputs.email}
+                  name="email"
+                  onChange={handleEmailChange}
+                  placeholder="email@example.com"
+                  className="field-empty"
+                />
+                <p className="warning-text"><i>Looks like this is not an email</i></p>
+              </div>
+          }
+          {
+            (inputs.password !== '')
+              ? <div>
+                <input
+                  type="password"
+                  value={inputs.password}
+                  name="password"
+                  onChange={handleInputChange}
+                />
+              </div>
+              : <div>
+                <input
+                  type="password"
+                  value={inputs.password}
+                  name="password"
+                  onChange={handleInputChange}
+                  className="field-empty"
+                />
+                <p className="warning-text"><i>Password cannot be empty</i></p>
+              </div>
+          }
+          <input type="submit" value="CLAIM YOUR FREE TRIAL"/>
         </form>
     )
 }
